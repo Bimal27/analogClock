9ea50238af9823758a3a851753fcaba88f4f2e60
Bimal27 <bimalsah1996@gmail.com>
2023-05-27 20:50:49 +0200

    added files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [time, setTime] = useState({
+    hourNeedle: 0,
+    minNeedle: 0,
+    secondNeedle: 0,
+    actualDate: "",
+    month: "",
+    year: ""
+  });
+
+  const numbers = Array.from({ length: 12 }, (_, index) => index + 1);
+
+  function displayTime() {
+    let date = new Date();
+
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+    let actualDate = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    let hourNeedle = 30 * hh + mm / 2;
+    let minNeedle = 6 * mm;
+    let secondNeedle = 6 * ss;
+
+    setTime({ hourNeedle, minNeedle, secondNeedle, actualDate, month, year });
+  }
+
+  useEffect(() => {
+    const interval = setInterval(displayTime, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <div className="clock">
+        <div className="date">
+          {time.actualDate}/
+          {time.month}
+          /{time.year}
+        </div>
+
+        <div class="pendulum">
+          <div class="rod" />
+          <div class="circle" />
+        </div>
+        <div
+          style={{ transform: `rotate(${time.hourNeedle}deg)` }}
+          className="hand"
         >
-          Learn React
-        </a>
-      </header>
+          <i style={{ height: "110px", width: "6px" }} />
+        </div>
+        <div
+          style={{ transform: `rotate(${time.minNeedle}deg)` }}
+          className="hand"
+        >
+          <i style={{ height: "140px", width: "4px" }} />
+          <i />
+        </div>
+        <div
+          className="hand"
+          style={{ transform: `rotate(${time.secondNeedle}deg)` }}
+        >
+          <i style={{ height: "180px", width: "2px" }} />
+          <i />
+        </div>
+
+        {numbers.map(number =>
+          <span key={number} style={{ "--num": number }}>
+            <b>
+              {number}
+            </b>
+          </span>
+        )}
+      </div>
     </div>
   );
 }
